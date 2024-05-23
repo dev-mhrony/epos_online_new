fa1ed85191838386412f0346d55a5e37823bf6c7
dev-mhrony <mh.rony.dhanvi@gmail.com>
2024-05-23 11:55:28 +0600

    first commit

---
+++ assets/plugin/select2/dist/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    $(".js-select2").select2();
+
+    $(".js-select2-multi").select2();
+
+    $(".large").select2({
+        dropdownCssClass: "big-drop",
+    });
+
+});
